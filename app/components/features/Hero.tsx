b920665ce052e7ca76b437716e7c3e70dce7e488
fuuta0201 <satofuta0201@gmail.com>
2024-05-30 22:23:49 +0900

    feat: hero link

--- app/components/features/Hero.tsx
+++ app/components/features/Hero.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Hero = () => {
   return (
     <section className='w-full h-dvh h-vh flex flex-col items-center justify-center gap-y-[100px]'>
       <h2 className='text-6xl font-bold'>Futa Sato's Portfolio Web Site</h2>
-      <div className='flex items-center'>
+      <Link href={"#about"} className='flex items-center'>
         <p className='text-3xl font-bold'>View this site</p>
         <Image src={"/images/icons/chevron-down.svg"} width={40} height={40} alt='' />
-      </div>
+      </Link>
     </section>
   )
 }
